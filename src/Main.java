b80f28c05dcc18398192a2f1168718617f0346b9
DayvisM <daviesmatthewa@gmail.com>
2022-09-29 07:46:13 +0100

    even numbers to 100

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+
+        System.out.println("Find below the list of even numbers from 0 - 100:");
+        for (int i = 2; i <= 100; i = i + 2)
+        {
+            System.out.println(i);
+        }
+
+    }
+}
